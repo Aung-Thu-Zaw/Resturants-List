bc5f92ed6b69fd58b97a11f4c2ac1a4d08847192
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2022-10-30 09:15:43 +0630

    create custom 404 page

---
+++ pages/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container text-center mt-5">
+      <h1>Page Not Found</h1>
+      <a href="/restaurants">Go Back</a>
+    </div>
+  </div>
+</template>  
